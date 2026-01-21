5cc77e4681bea63527215aefc1fb156922d50d8a
Rafael Borges <rafaborgesdasilva75@gmail.com>
2026-01-21 15:22:10 -0300

    Add Task class with properties for task management

---
+++ Task.cs
@@ -0,0 +1,21 @@
+using System;
+
+public class Task
+{
+	public int id { get; set; }
+    public string title { get; set; }
+    public string description { get; set; }
+    public DateTime startDate { get; set; }
+    public DateTime endDate { get; set; }
+
+	/*
+    public Task(int id, string title, string description, DateTime startDate, DateTime endDate)
+	{
+		this.id = id;
+		this.title = title;
+		this.description = description;
+		this.startDate = startDate;
+		this.endDate = endDate;
+	}
+	*/
+}
