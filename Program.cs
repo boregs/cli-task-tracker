0128c6e9b439290f955a21bb9f45cb2cd508c821
Rafael Borges <rafaborgesdasilva75@gmail.com>
2026-01-22 13:29:24 -0300

    first draft of the CLI interface
    Added CLI interface for task management options.

--- Program.cs
+++ Program.cs
@@ -4,6 +4,33 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        DataAcess.addTask();
+        Console.WriteLine("============================================================================");
+        Console.WriteLine("========================== CLI Task Orginizer ==============================");
+        Console.WriteLine("============================================================================");
+
+        Console.WriteLine("");
+        Console.WriteLine("----------------------------------------------------------------------------");
+        Console.WriteLine(">> What do you want to do?");
+        Console.WriteLine("> 1 - Check Tasks");
+        Console.WriteLine("> 2 - Add Task");
+        Console.WriteLine("> 3 - Delete Task");
+
+        string userInput = Console.ReadLine();
+
+        if (userInput == null) {
+            Console.WriteLine(">> ERROR: Need to choose a option");
+        } else if (userInput == "1") { 
+
+        }
+        else if (userInput == "2")
+        {
+
+        }
+        else if (userInput == "3")
+        {
+
+        }
+
+        Console.ReadKey();
     }
 }
