03d58dbc569754c8f777de323e33bd225c5a9769
Rafael Borges <rafaborgesdasilva75@gmail.com>
2026-01-23 11:58:38 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,7 +4,40 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        /*
+        while (true)
+        {
+            Introduction();
+
+            string userInput = Console.ReadLine();
+
+            if (userInput == null)
+            {
+                Console.WriteLine(">> ERROR: You need to choose a option");
+            }
+            else if (userInput == "1")
+            {
+                DataAcess.ReadTask();
+            }
+            else if (userInput == "2")
+            {
+                DataAcess.AddTask();
+            }
+            else if (userInput == "3")
+            {
+
+            }
+            else if (userInput == "4")
+            {
+                break;
+            }
+
+        }
+
+        
+    }
+
+    public static void Introduction()
+    {
         Console.WriteLine("============================================================================");
         Console.WriteLine("========================== CLI Task Orginizer ==============================");
         Console.WriteLine("============================================================================");
@@ -15,30 +48,6 @@ public class Program
         Console.WriteLine("> 1 - Check Tasks");
         Console.WriteLine("> 2 - Add Task");
         Console.WriteLine("> 3 - Delete Task");
-
-        string userInput = Console.ReadLine();
-
-        if (userInput == null) {
-            Console.WriteLine(">> ERROR: Need to choose a option");
-        } else if (userInput == "1") { 
-
-        }
-        else if (userInput == "2")
-        {
-
-        }
-        else if (userInput == "3")
-        {
-
-        }
-
-        Console.ReadKey();
-        */
-
-        long randLong = RandNumberGen.rand();
-
-        Console.WriteLine(TaskIDGenerator.GenerateId(randLong));
-
-
+        Console.WriteLine("> 4 - Leave");
     }
 }
