1a4fb5f998b3c187f250a006ce1db58c2cab249e
Rafael Borges <rafaborgesdasilva75@gmail.com>
2026-01-23 14:53:41 -0300

    added ReadTask method prototype

--- DataAccess.cs
+++ DataAccess.cs
@@ -59,8 +59,58 @@ public class DataAcess
 		
     }
 
-	public static void readTask()
+	
+	// need to fix this method
+	public static void ReadTask()
 	{
+	
+	    Dictionary<string, string[]> taskKeeper = new Dictionary<string, string[]>();
+	    long randLong = RandNumberGen.Rand();
+	    string keyID = TaskIDGenerator.GenerateId(randLong);
+	
+	    Console.WriteLine(">> Do you want to see all tasks? (Y/n)");
+	    Console.WriteLine(">> Y - Yes (Recommended)");
+	    Console.WriteLine(">> n - No");
+	    
+		string userInput = Console.ReadLine().ToLower();
+	
+	    if (userInput == "y")
+	    {
+	        try
+	        {
+	            foreach (KeyValuePair<string, string[]> entry in taskKeeper)
+	            {
+	                string key = entry.Key;
+	                string[] values = entry.Value;
+	
+	                Console.Write($"ID: {key}  |  ");
+	                Console.Write("Contents: ");
+	                Console.Write($"[{string.Join(" | ", values)}]");
+	            }
+	
+	        }
+	        catch (Exception ex)
+	        {
+	            Console.WriteLine($">> ERROR: {ex.Message}");
+	        }
+	    }
+	    else if (userInput == "n") {
+	
+	        Console.WriteLine(">> Please Inform the ID of the task:");
+	        Console.WriteLine(">> NOTE: Please use the exact ID");
+	        Console.WriteLine(">");
+	        string taskID = Console.ReadLine();
+	        
+	        // searches the ID(key) on the hashmap
+	        if (taskKeeper.ContainsKey(taskID))
+	        {
+	            Console.WriteLine($"Task {taskID} found. Content: {taskKeeper[taskID]}");
+	        }
+	    }
+	    else
+	    {
+	        Console.WriteLine(">> ERROR: Please choose an available option");
+	    }
 	}
 
 	public static void updateTask()
